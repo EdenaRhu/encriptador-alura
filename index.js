8e50e316344eedd0ac77a3ea79acce50797be339
Kings <jdlreyes1604@gmail.com>
2024-02-26 22:02:40 -0600

    adding js functions encrypt and decrypt

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ function asigneElementByText(elemento, texto){
 
 function getUserText(){
     let userText = document.querySelector('.userarea__textarea').value.toLowerCase();
-    alert(userText);
     muneco.classList.add("ocultar");
     userTitulo.classList.add("ocultar");
-    asigneElementByText(".whitebox__p", userText);
     btnCopy.classList.remove("ocultar");
-    
+    asigneElementByText(".whitebox__p", encryptUserText(userText));
 }
 
+
+function encryptUserText(textUser){
+    return textUser.replace(/e/g, "enter")
+                    .replace(/i/g, "imes")
+                    .replace(/a/g, "ai")
+                    .replace(/o/g, "ober")
+                    .replace(/u/g, "ufat");
+}
+
+function decryptUserText(textUser){
+    return text.replace(/enter/g, "e")
+                .replace(/imes/g, "i")
+                .replace(/ai/g, "a")
+                .replace(/ober/g, "o")
+                .replace(/ufat/g, "u");
+}
 
